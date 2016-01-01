ef758ff610a03782cc0e065a8472e71df9059ff2
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-01-01 16:31:12 +0530

    Initial Commit

---
+++ Map Integration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Integration
+//
+//  Created by Chatterjee, Snigdhaman on 01/01/16.
+//  Copyright © 2016 Chatterjee, Snigdhaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
